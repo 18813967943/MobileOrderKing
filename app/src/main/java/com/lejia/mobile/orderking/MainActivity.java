0b87c9abc90727e81479c1a263acf629e68d24b6
bxw <bao_xiong_wei@126.com>
2018-06-30 18:06:41 +0800

    2018.6.30

--- app/src/main/java/com/lejia/mobile/orderking/MainActivity.java
+++ app/src/main/java/com/lejia/mobile/orderking/MainActivity.java
@@ -2,13 +2,15 @@ package com.lejia.mobile.orderking;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends Activity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.e("TAG","hijgijgre");
+
     }
 
 }
