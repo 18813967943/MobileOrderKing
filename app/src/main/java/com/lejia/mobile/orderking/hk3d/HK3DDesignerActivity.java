0d1a595783e4214873b71c4c230db576866af245
heke <526051472@qq.com>
2018-07-02 10:11:26 +0800

    更新目录结构及配置开发环境

---
+++ app/src/main/java/com/lejia/mobile/orderking/hk3d/HK3DDesignerActivity.java
@@ -0,0 +1,21 @@
+package com.lejia.mobile.orderking.hk3d;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Author by HEKE
+ *
+ * @time 2018/7/2 9:52
+ * TODO: 三维设计界面
+ */
+public class HK3DDesignerActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+    }
+
+}
