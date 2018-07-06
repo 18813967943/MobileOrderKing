b0af027cd9986942382550d8be8787b93931ff3e
heke <526051472@qq.com>
2018-07-06 11:59:54 +0800

    更新登录界面

--- app/src/main/java/com/lejia/mobile/orderking/activitys/LoginActivity.java
+++ app/src/main/java/com/lejia/mobile/orderking/activitys/LoginActivity.java
@@ -1,8 +1,16 @@
 package com.lejia.mobile.orderking.activitys;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.lejia.mobile.orderking.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Author by HEKE
@@ -12,6 +20,16 @@ import com.lejia.mobile.orderking.R;
  */
 public class LoginActivity extends BaseActivity {
 
+    @BindView(R.id.account)
+    EditText account;
+    @BindView(R.id.password)
+    EditText password;
+    @BindView(R.id.forgetPassword)
+    TextView forgetPassword;
+    @BindView(R.id.login)
+    Button login;
+    @BindView(R.id.register)
+    Button register;
 
     @Override
     protected void initViews() {
@@ -23,6 +41,19 @@ public class LoginActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         super.setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.forgetPassword, R.id.login, R.id.register})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.forgetPassword:
+                break;
+            case R.id.login:
+                break;
+            case R.id.register:
+                break;
+        }
     }
 
 }
